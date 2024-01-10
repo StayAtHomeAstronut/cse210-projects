1e513c07b7167627eb919b0329e3cd2950e455d8
Sean Porter <por22009@byui.edu>
2024-01-10 12:19:57 -0700

    Completed Learning Activity C# 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        float percentage = float.Parse(Console.ReadLine());
+        string letter;
+
+        if (percentage >=90)
+        {
+            letter = "n A";
+        }
+        else if (percentage>=80)
+        {
+            letter = " B";
+        }
+        else if (percentage>=70)
+        {
+            letter = " C";
+        }
+        else if (percentage >=60)
+        {
+            letter = " D";
+        }
+        else
+        {
+            letter = "n F";
+        }
+
+        Console.WriteLine($"Your Grade is a{letter}.");
+
+        if (percentage >=70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else{
+            Console.WriteLine("Unfortunately, you did not pass. Try again!");
+        }
     }
 }
